5005bfcbfedd89dfab7047b9d7ead6e15a6eac6c
CristianL <cristian.lizarazoc@udea.edu.co>
2025-09-15 14:38:06 -0500

    feat: build architecture

---
+++ src/main/java/com/udea/CourierSync/CourierSyncApplication.java
@@ -0,0 +1,13 @@
+package com.udea.CourierSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/CourierSync/CourierSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.CourierSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
